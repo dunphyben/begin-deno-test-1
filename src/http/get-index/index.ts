fbd6a29a0eaed6f55a1d004ac144d185a03e546f
Benjamin Dunphy <dunphy.ben@gmail.com>
2020-06-08 10:55:50 -0700

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -12,7 +12,7 @@ let body = `
 
     <h1 class="center-text">
       <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+      Hola Mundo!!
     </h1>
 
     <p class="center-text">
